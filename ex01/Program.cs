ca8fad556f8b74f74eb7073a311d230432cbc93b
AndreySh <shingaryovan@gmail.com>
2023-02-03 11:49:59 +1200

    Показать двумерный массив размером m×n заполненный вещественными числами

---
+++ ex01/Program.cs
@@ -0,0 +1,34 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+Console.Write("Введите количество строк матрицы: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы: ");
+int n = int.Parse(Console.ReadLine());
+double[,] matrix = new double[m,n];
+
+void PrintMatrix(double[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            Console.Write($" {arg[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FillMatrix(double[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            arg[i,j] = new Random().NextDouble() * 20 - 10;
+        }
+    }
+}
+
+FillMatrix(matrix);
+
+PrintMatrix(matrix);
