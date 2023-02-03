eba94a6b45cd67c5ea48dcb27c52243afa866eda
AndreySh <shingaryovan@gmail.com>
2023-02-03 12:02:34 +1200

    Задать двумерный массив следующим правилом: Aₘₙ = m+n

---
+++ ex02/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+Console.Write("Введите количество строк матрицы: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[m,n];
+
+void PrintMatrix(int[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            Console.Write($" {arg[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FillMatrix(int[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            arg[i,j] = i + j;
+        }
+    }
+}
+
+FillMatrix(matrix);
+
+PrintMatrix(matrix);
